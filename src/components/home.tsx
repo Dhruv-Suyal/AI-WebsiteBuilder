faad37e52ccc58607716e58cbab7c9dcd1079864
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2026-01-27 21:28:58 +0530

    made radial gradient

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,6 +5,7 @@ export function Home(){
         <>
         <title>Home</title>
         <Header/>
+        <div className=" absolute -top-220 -left-10 z-50 h-400 w-400 overflow-x-hidden rounded-b-[1800px] bg-[radial-gradient(circle,var(--color-black)_30%,var(--color-indigo-700)_80%)]"></div>
         </>
     )
 }
