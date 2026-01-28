135d20f5ad16ffd7e7fdf0070e1b5f027229b5ec
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2026-01-28 14:59:19 +0530

    Complete the radial gradient

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,7 +5,7 @@ export function Home(){
         <>
         <title>Home</title>
         <Header/>
-        <div className=" absolute -top-220 -left-10 z-50 h-400 w-400 overflow-x-hidden rounded-b-[1800px] bg-[radial-gradient(circle,var(--color-black)_30%,var(--color-indigo-700)_80%)]"></div>
+        <div className="absolute -top-240 -left-18 z-50 h-400 w-420 overflow-x-hidden rounded-b-[1800px] bg-[radial-gradient(circle,var(--color-black)_30%,var(--color-indigo-700)_80%)]"></div>
         </>
     )
 }
