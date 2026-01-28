3655db5ad298882d8250d9c0b8e529dd9c158ee5
Dhruv-Suyal <dhruvsuyal0@gmail.com>
2026-01-28 19:29:38 +0530

    make responsive radial gradient

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,7 +5,9 @@ export function Home(){
         <>
         <title>Home</title>
         <Header/>
-        <div className="absolute -top-240 -left-18 z-50 h-400 w-420 overflow-x-hidden rounded-b-[1800px] bg-[radial-gradient(circle,var(--color-black)_30%,var(--color-indigo-700)_80%)]"></div>
+        <div className=" flex items-center justify-center">
+        <div className="fixed -top-180 sm:-top-160 md:-top-270 h-260 w-250 md:h-400 md:w-300 lg:-top-240 z-50 lg:h-400 xl:h-400 xl:w-450 lg:w-350 overflow-x-hidden rounded-b-[1800px] bg-[radial-gradient(circle,var(--color-black)_40%,var(--color-indigo-700)_80%)]"></div>
+        </div>
         </>
     )
 }
